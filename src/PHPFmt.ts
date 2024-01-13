6cb25da48589c441e31f9717e195dd8a9214f561
SenPai <dev.alix13@gmail.com>
2024-01-13 15:56:05 +0330

    support PHP 8.3.1 (#136)
    * support PHP 8.3.1
    
    * Update PHPFmt.ts

--- src/PHPFmt.ts
+++ src/PHPFmt.ts
@@ -222,9 +222,9 @@ export class PHPFmt {
       } else {
         if (
           compare(phpVersion, '5.6.0', '<') ||
-          compare(phpVersion, '8.3.0', '>')
+          compare(phpVersion, '8.4.0, '>')
         ) {
-          throw new PHPFmtError('PHP version < 5.6 or > 8.3');
+          throw new PHPFmtError('PHP version < 5.6 or > 8.4');
         }
       }
     } catch (err) {
